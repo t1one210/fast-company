72c9179ea8f313eb5324136710012da0fe5835e2
t1one210 <tionev.andrew@gmail.com>
2023-08-06 16:35:56 +0400

    react-redux create and delete comments

--- src/app/store/createStore.js
+++ src/app/store/createStore.js
@@ -2,11 +2,13 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import qualitiesReducer from "./qualities";
 import professionReducer from "./professions";
 import usersReducer from "./users";
+import commentsReducer from "./comments";
 
 const rootReducer = combineReducers({
     qualities: qualitiesReducer,
     professions: professionReducer,
-    users: usersReducer
+    users: usersReducer,
+    comments: commentsReducer
 });
 
 export function createStore() {
--- src/app/store/users.js
+++ src/app/store/users.js
@@ -4,6 +4,7 @@ import authService from "../services/auth.service";
 import localStorageService from "../services/localStorage.service";
 import getRandomInt from "../utils/getRandomInt";
 import history from "../utils/history";
+import { generateAuthError } from "../utils/genereteAuthError";
 
 const initialState = localStorageService.getAccessToken()
     ? {
@@ -62,6 +63,9 @@ const usersSlice = createSlice({
             state.entities[
                 state.entities.findIndex((u) => u._id === action.payload._id)
             ] = action.payload;
+        },
+        authRequested: (state) => {
+            state.error = null;
         }
     }
 });
@@ -95,7 +99,13 @@ export const logIn =
             localStorageService.setTokens(data);
             history.push(redirect);
         } catch (error) {
-            dispatch(authRequestFailed(error.message));
+            const { code, message } = error.response.data.error;
+            if (code === 400) {
+                const errorMessage = generateAuthError(message);
+                dispatch(authRequestFailed(errorMessage));
+            } else {
+                dispatch(authRequestFailed(error.message));
+            }
         }
     };
 
@@ -178,5 +188,5 @@ export const getIsLoggedIn = () => (state) => state.users.isLoggedIn;
 export const getDataStatus = () => (state) => state.users.dataLoaded;
 export const getUsersLoadingStatus = () => (state) => state.users.isLoading;
 export const getCurrentUserId = () => (state) => state.users.auth.userId;
-
+export const getAuthErrors = () => (state) => state.users.error;
 export default usersReducer;
